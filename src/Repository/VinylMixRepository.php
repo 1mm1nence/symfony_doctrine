5d537f232fd7b13effa941a6e616c985c432f3a4
1mm1nence <110335004+1mm1nence@users.noreply.github.com>
2023-11-12 00:21:30 +0200

    added pagination with BabDev\PagerfantaBundle

--- src/Repository/VinylMixRepository.php
+++ src/Repository/VinylMixRepository.php
@@ -55,10 +55,8 @@ class VinylMixRepository extends ServiceEntityRepository
 //        ;
 //    }
 
-   /**
-    * @return VinylMix[] Returns an array of VinylMix objects
-    */
-   public function findAllOrderedByVotes(string $genre = null): array
+
+   public function createOrderedByVotesQueryBuilder(string $genre = null): QueryBuilder
    {
        $queryBuilder = $this->addOrderByVotesQueryBuilder();
         if ($genre) {
@@ -66,9 +64,7 @@ class VinylMixRepository extends ServiceEntityRepository
                 ->setParameter('genre', $genre);
         }
 
-       return $queryBuilder
-            ->getQuery()
-            ->getResult();
+       return $queryBuilder;
    }
 
    private function addOrderByVotesQueryBuilder(QueryBuilder $queryBuilder = null): QueryBuilder
